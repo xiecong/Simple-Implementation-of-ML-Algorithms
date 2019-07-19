69442ecb22b181c0e1986ef16c561bd521e61bfc
xiecong <xiecng@gmail.com>
2019-07-19 01:59:24 -0400

    implement enumerate solver of bayesian network

---
+++ svm.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class SVM(object):
+    def __init__(self, x, y):
+    	pass
+
+    def fit(self):
+    	pass
+
+    def predict(self, x):
+    	pass
+
+def main():
+	pass
+
+if __name__ == "__main__":
+    main()
